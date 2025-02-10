185d2f1762434abe243d5939b13baae4f2fab9dc
arasu20 <imarasu20@gmail.com>
2025-02-10 20:32:28 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
+import Working from "./components/Working"
 
 const  App = ()=> {
 
@@ -11,6 +14,9 @@ const  App = ()=> {
     <Hero/>
     <Highlights/>
     <Model/>
+    <Features/>
+    <Working/>
+    <Footer/>
   </main>
   )
 }
